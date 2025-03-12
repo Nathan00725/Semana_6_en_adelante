51acd465309446d9f80d947a210bf829816ed65f
Nathan00725 <lmnathan25@gmail.com>
2025-03-12 13:54:22 -0600

    Commit
    Agregando todo los npm y npx de android

---
+++ Aprender/Practica/android/app/src/main/java/com/Shadow/DV2/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.Shadow.DV2.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- Aprender/Practica/capacitor.config.ts
+++ Aprender/Practica/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Practica',
+  appId: 'com.Shadow.DV2.app',
+  appName: 'Shadow',
   webDir: 'www'
 };
 
